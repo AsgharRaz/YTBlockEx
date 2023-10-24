52103c5a1839f9eb64200bf8d6c8c42b225b4787
M K <n3rv@hotmail.de>
2023-10-25 01:09:51 +0200

    fix more edge cases

--- ytblockex/content.js
+++ ytblockex/content.js
@@ -6,27 +6,33 @@ sources = () => [
 ]
 
 players = () => [
-  '#ytd-player',
+  '#player-full-bleed-container',
   'yt-playability-error-supported-renderers',
-  'ytd-enforcement-message-view-model'
+  'ytd-enforcement-message-view-model',
+  '#ytd-player'
 ]
 
-source = () => {
-  return sources().map(selector => document.querySelector(selector)?.content).find(source => !!source)
-}
-
-video = () => {
-  return players().map(selector => document.querySelector(selector)).find(source => !!source)
+buildUrl = () => {
+  const videoId = new URLSearchParams(window.location.search).get("v")
+  console.log("videoId", videoId)
+  return `https://www.youtube.com/embed/${videoId}`
 }
 
 unblock = () => {
-  const iframe = document.createElement("iframe")
-  iframe.style = "width: 100%; height: 100%"
-  iframe.title = "unblocked video"
-  iframe.frameborder = "0"
-  iframe.src = source()
-  video().replaceWith(iframe)
+  let player = players().map(selector => document.querySelector(selector)).find(source => !!source)
+  let source = sources().map(selector => document.querySelector(selector)?.content).find(source => !!source) || buildUrl()
+
+  if (player && source) {
+    const iframe = document.createElement("iframe")
+    iframe.style = "width: 100%; height: 100%"
+    iframe.title = "unblocked video"
+    iframe.frameborder = "0"
+    iframe.src = source
+    player.replaceWith(iframe)
+    console.log(`YTBlockEx: UNLOCKED video! source: ${source}, player: ${player.tagName} ${player.id}`)
+  } else {
+    console.error(`YTBlockEx: failed to find either player or source - source: ${source}, player: ${player}`)
+  }
 }
 
 unblock()
-
